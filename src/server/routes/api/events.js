592ea3e817c239f56a79a00bfdfff258c468fe18
DuyTheCoGa <ntminhduy123@gmail.com>
2021-01-10 11:35:48 +0700

    JSON AS U FUKING WANT

---
+++ src/server/routes/api/events.js
@@ -0,0 +1,181 @@
+const express = require('express');
+var serviceAccount = require("../../server-key.json");
+const sql = require("mssql/msnodesqlv8");
+
+var jsonParser = express.json();
+
+var router = express.Router();
+
+const conn = new sql.ConnectionPool({
+    database: "ServerDB",
+    server: "localhost",
+    driver: "msnodesqlv8",
+    options: {
+        trustedConnection: true,
+        enableArithAbort: true
+    },
+    port: 8883
+});
+
+
+router.param('eventId', function (req, res, next, id) {
+    console.log(id);
+    next()
+})
+
+
+// connect to database
+conn.connect().then(() => {
+    console.log('Connected!');
+});
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+//query table UserAccount
+router.get('/getevent', function (req, res) {
+    var queryString = `select * from Event`;
+    var second = false;
+
+    for (const key in req.query) {
+        console.log(key, req.query[key])
+        if (!second) {
+            queryString += `where ${key} = '${req.query[key]}'`;
+            second = true;
+        }
+        else
+            queryString += `AND ${key} = '${req.query[key]}'`;
+    }
+    //queryString += ` LEFT JOIN UserAccount ON UserAccount.IDandPrefix = Event.HostID`;
+
+    var jsonResult = { 'title': 'The queried events' };
+
+    conn.query(queryString)
+        .then(result => {
+            jsonResult.users = result;
+
+            var j = 0;
+            for (i in Object.entries(jsonResult.users.recordset)) {
+                var hostQuery = `Select * from UserAccount Where IDandPrefix = '${jsonResult.users.recordset[j].HostID}'`
+                conn.query(hostQuery)
+                    .then(result => {
+                        jsonResult.users.recordset[j].HostID = result.recordset;
+                        j++;
+                        if (j == jsonResult.users.recordset.length){
+                            res.send(jsonResult);
+                        }
+                    }).catch(err => {
+                        // ... error checks
+                        if (err) {
+                            console.error('Error !!!');
+                            throw err
+                        }
+                    });
+                
+            } 
+        }).catch(err => {
+            // ... error checks
+            if (err) {
+                console.error('Error !!!');
+                throw err
+            }
+        });
+});
+
+router.get('/getfollowusers/:eventId', function (req, res) {
+    const eventId = req.params.eventId;
+    var queryString = `select * from Follow 
+    INNER JOIN UserAccount ON UserAccount.IDandPrefix = Follow.UserID
+    WHERE Follow.EventID = '${eventId}';`
+
+
+    conn.query(queryString)
+        .then(result => {
+            res.send({
+                'title': 'The queried followed users',
+                'result': result
+            });
+        }).catch(err => {
+            // ... error checks
+            if (err) {
+                console.error('Error !!!');
+                throw err
+            }
+        });
+});
+
+router.post('/createevent', (req, res) => {
+    var queryString = "INSERT INTO Event ";
+    var cols, values;
+    cols = "(";
+    values = "(";
+    var second = false;
+
+    for (const [key, value] of Object.entries(req.body)) {
+        if (!second) {
+            cols += `${key}`;
+            values += `${value}`;
+            second = true;
+        }
+        else {
+            cols += `,${key}`;
+            values += `,${value}`;
+        }
+
+    }
+    cols += ')';
+    values += ')';
+
+    queryString += cols + " VALUES " + values;
+    console.log(queryString)
+
+    var transaction = new sql.Transaction(conn);
+    transaction.begin().then(function () {
+        conn.query(queryString)
+            .then(function () {
+                transaction.commit().then(function (recordSet) {
+                    console.log(recordSet);
+                    conn.close();
+                    return res.send('Inserted successfully');
+                }).catch(function (err) {
+                    console.log("Error in Transaction Commit " + err);
+                    conn.close();
+                    return res.send('Error');
+                });
+            });
+    });
+});
+
+
+router.patch('/patchevent/:eventId', jsonParser, (req, res) => {
+    const eventId = req.params.eventId;
+
+    var queryString = "UPDATE Event ";
+    var second = false;
+
+    for (const [key, value] of Object.entries(req.body)) {
+        if (!second) {
+            queryString += `SET ${key} = '${value}'`;
+            second = true;
+        }
+        else
+            queryString += `, ${key} = '${value}'`;
+    }
+    queryString += " WHERE IDandPrefix = '" + eventId + "';"
+    console.log(queryString)
+
+    conn.query(queryString, function (err, data) {
+        var result = {};
+        if (err) {
+            conn.release();
+        } else {
+            conn.release();
+            result = data;
+        }
+    });
+    res.end()
+});
+
+module.exports = router;
---
+++ src/server/routes/api/users.js
@@ -0,0 +1,181 @@
+const express = require('express');
+var serviceAccount = require("../../server-key.json");
+const sql = require("mssql/msnodesqlv8");
+const { QueryEvent } = require('msnodesqlv8');
+
+var jsonParser = express.json();
+
+var router = express.Router();
+
+const conn = new sql.ConnectionPool({
+    database: "ServerDB",
+    server: "localhost",
+    driver: "msnodesqlv8",
+    options: {
+        trustedConnection: true,
+        enableArithAbort: true
+    },
+    port: 8883
+});
+
+// connect to database
+conn.connect().then(() => {
+    console.log('Connected!');
+});
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.param('userId', function (req, res, next, id) {
+    console.log(id);
+    next()
+})
+
+//query table UserAccount
+router.get('/getuser', function (req, res) {
+    var queryString = `select * from UserAccount `;
+    var second = false;
+
+    for (const key in req.query) {
+        console.log(key, req.query[key])
+        if (!second) {
+            queryString += `where ${key} = '${req.query[key]}'`;
+            second = true;
+        }
+        else
+            queryString += `AND ${key} = '${req.query[key]}'`;
+    }
+
+    conn.query(queryString)
+        .then(result => {
+            res.send({
+                'title': 'The queried user',
+                'result': result
+            });
+        }).catch(err => {
+            // ... error checks
+            if (err) {
+                console.error('Error !!!');
+                throw err
+            }
+        });
+});
+
+//get followed event
+router.get('/getfollowevents/:userId', function (req, res) {
+    const userId = req.params.userId;
+    var queryString = `select * 
+    from Follow 
+    INNER JOIN Event ON Event.IDandPrefix = Follow.EventID
+    WHERE Follow.UserID = '${userId}';`
+
+    var jsonResult = { 'title': 'The queried events' };
+
+    conn.query(queryString)
+        .then(result => {
+            jsonResult.users = result;
+
+            var j = 0;
+            for (i in Object.entries(jsonResult.users.recordset)) {
+                var hostQuery = `Select * from UserAccount Where IDandPrefix = '${jsonResult.users.recordset[j].HostID}'`
+                conn.query(hostQuery)
+                    .then(result => {
+                        jsonResult.users.recordset[j].HostID = result.recordset;
+                        j++;
+                        if (j == jsonResult.users.recordset.length) {
+                            res.send(jsonResult);
+                        }
+                    }).catch(err => {
+                        // ... error checks
+                        if (err) {
+                            console.error('Error !!!');
+                            throw err
+                        }
+                    });
+
+            }
+        }).catch(err => {
+            // ... error checks
+            if (err) {
+                console.error('Error !!!');
+                throw err
+            }
+        });
+});
+
+
+router.post('/createuser', jsonParser, (req, res) => {
+    console.log(req.body);
+    var queryString = "INSERT INTO UserAccount ";
+    var cols, values;
+    cols = "(";
+    values = "(";
+    var second = false;
+
+    for (const [key, value] of Object.entries(req.body)) {
+        if (!second) {
+            cols += `${key}`;
+            values += `${value}`;
+            second = true;
+        }
+        else {
+            cols += `,${key}`;
+            values += `,${value}`;
+        }
+
+    }
+    cols += ')';
+    values += ')';
+
+    queryString += cols + " VALUES " + values;
+    console.log(queryString)
+
+    var transaction = new sql.Transaction(conn);
+    transaction.begin().then(function () {
+        conn.query(queryString)
+            .then(function () {
+                transaction.commit().then(function (recordSet) {
+                    console.log(recordSet);
+                    conn.close();
+                    return res.send('Inserted successfully');
+                }).catch(function (err) {
+                    console.log("Error in Transaction Commit " + err);
+                    conn.close();
+                    return res.send('Error')
+                });
+            });
+    });
+});
+
+router.patch('/patchuser/:userId', jsonParser, (req, res) => {
+    const userId = req.params.userId;
+
+    var queryString = "UPDATE UserAccount ";
+    var second = false;
+
+    for (const [key, value] of Object.entries(req.body)) {
+        if (!second) {
+            queryString += `SET ${key} = '${value}'`;
+            second = true;
+        }
+        else
+            queryString += `, ${key} = '${value}'`;
+    }
+    queryString += " WHERE IDandPrefix = '" + userId + "';"
+    console.log(queryString)
+
+    conn.query(queryString, function (err, data) {
+        var result = {};
+        if (err) {
+            conn.release();
+        } else {
+            conn.release();
+            result = data;
+        }
+    });
+    res.end()
+});
+
+module.exports = router;
