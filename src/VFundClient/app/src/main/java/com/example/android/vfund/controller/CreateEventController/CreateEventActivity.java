d7c578c92d51d5559307f75182fb14ad73619638
Kim Nguyễn Duy Thiên <ngduykim@gmail.com>
2021-01-14 20:29:41 +0700

    Updates to CreateEventActivity

--- src/VFundClient/app/src/main/java/com/example/android/vfund/controller/CreateEventController/CreateEventActivity.java
+++ src/VFundClient/app/src/main/java/com/example/android/vfund/controller/CreateEventController/CreateEventActivity.java
@@ -1,38 +1,95 @@
 package com.example.android.vfund.controller.CreateEventController;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import com.example.android.vfund.R;
 import com.example.android.vfund.model.FundraisingEvent;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CreateEventActivity extends AppCompatActivity {
+
 
     LinearLayout firstPage, secondPage, lastPage;
     TextInputLayout txtEventName, txtEventStory, txtEventMoneyAmount, txtUserName, txtCardNumber;
     TextInputLayout txtAddress, txtPhone;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
         Toolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
+
+        final LinearLayout firstPage = findViewById(R.id.firstPage);
+        final LinearLayout secondPage = findViewById(R.id.secondPage);
+        final LinearLayout lastPage = findViewById(R.id.lastPage);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firstPage.getVisibility() == View.VISIBLE)
+                    finish();
+                else if (secondPage.getVisibility() == View.VISIBLE)
+                {
+                    firstPage.setVisibility(View.VISIBLE);
+                    secondPage.setVisibility(View.INVISIBLE);
+                }
+                else if (lastPage.getVisibility() == View.VISIBLE)
+                {
+                    secondPage.setVisibility(View.VISIBLE);
+                    lastPage.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        TextInputEditText eText;
+        eText=(TextInputEditText) firstPage.findViewById(R.id.txtDate);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                DatePickerDialog picker = new DatePickerDialog(CreateEventActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                TextInputEditText eText;
+                                cldr.set(year, monthOfYear, dayOfMonth);
+                                String pattern= "dd-MM-yyyy";
+                                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+                                String date = simpleDateFormat.format(cldr.getTime());
+                                eText=(TextInputEditText) firstPage.findViewById(R.id.txtDate);
+                                eText.setText(date);
+                                //eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
 
         FundraisingEvent event = new FundraisingEvent();
         txtEventName = findViewById(R.id.txtEventName);
@@ -62,7 +119,7 @@ public class CreateEventActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firstPage.setVisibility(View.GONE);
+                firstPage.setVisibility(View.INVISIBLE);
                 secondPage.setVisibility(View.VISIBLE);
             }
         });
@@ -71,7 +128,7 @@ public class CreateEventActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                secondPage.setVisibility(View.GONE);
+                secondPage.setVisibility(View.INVISIBLE);
                 lastPage.setVisibility(View.VISIBLE);
             }
         });
@@ -81,8 +138,46 @@ public class CreateEventActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 /* to-do: complete activity and send event to database */
+                String eventName, eventStory, eventMoney, eventDate, eventCategory;
+                TextInputEditText contents;
+
+                //first page contents
+                contents = firstPage.findViewById(R.id.txtEventName);
+                eventName = contents.getText().toString();
+
+                contents = firstPage.findViewById(R.id.txtEventStory);
+                eventStory = contents.getText().toString();
+
+                contents = firstPage.findViewById(R.id.txtEventMoneyAmount);
+                eventMoney = contents.getText().toString();
+
+                contents = firstPage.findViewById(R.id.txtDate);
+                eventDate = contents.getText().toString();
+
+                contents = firstPage.findViewById(R.id.autoComplete);
+                eventCategory = contents.getText().toString();
+
+                String name, cardNumber, address, phone, bank;
+                //second page contents
+                contents = secondPage.findViewById(R.id.txtName);
+                name = contents.getText().toString();
+
+                contents = secondPage.findViewById(R.id.txtCardNumber);
+                cardNumber = contents.getText().toString();
+
+                contents = secondPage.findViewById(R.id.txtAddress);
+                address = contents.getText().toString();
+
+                contents = secondPage.findViewById(R.id.txtPhone);
+                phone = contents.getText().toString();
+
+                contents = secondPage.findViewById(R.id.autoComplete);
+                bank = contents.getText().toString();
+
                 finish();
             }
         });
+
+
     }
 }
